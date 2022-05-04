650585ce30a85bc5d20a46424a215e65862fa922
Ricardo Anjos <ricardoanj.14@gmail.com>
2022-05-03 23:56:20 -0300

    insatall tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <h1>Hello World</h1>
+    <button className="">Hello World</button>
     </>
   )
 }
